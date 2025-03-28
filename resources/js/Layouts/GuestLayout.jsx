d54c8270b33988fcfcd36a8112790d9e9c4eb3de
John Cris <johncrismanabo9@gmail.com>
2025-03-28 14:27:04 +0800

    refactor: simplify GuestLayout structure and remove commented code

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,21 +3,22 @@ import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+    <div>
       {/* <div>
         <Link href="/">
           <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
         </Link>
       </div> */}
 
-      <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+      {/* <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
         {children}
       </div>
 
       <div className="mt-3 text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
         By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
         and <a href="#">Privacy Policy</a>.
-      </div>
+      </div> */}
+      {children}
     </div>
 
   );

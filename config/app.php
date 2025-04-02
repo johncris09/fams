e079a8469103d62d2becfc412395ef56de7ed2fa
John Cris <johncrismanabo9@gmail.com>
2025-04-02 15:08:45 +0800

    add project_full_name configuration to app settings

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'project_full_name' => env('APP_PROJECT_FULL_NAME', ''),
 
     /*
     |--------------------------------------------------------------------------
